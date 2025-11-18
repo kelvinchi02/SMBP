36000bee864c3c8fcc578e6715636dedab378d15
Kelvin Chi <kelvinchi2002@gmail.com>
2025-11-18 15:51:38 -0500

    new file:   main.R 	new file:   requiremeents.txt 	new file:   requirements_check.R

---
+++ main.R
@@ -0,0 +1,2 @@
+source("requirements_check.R")
+check_and_install_packages()
---
+++ requirements_check.R
@@ -0,0 +1,19 @@
+check_and_install_packages <- function(req_file = "requirements.txt") {
+  # Read list of packages
+  packages <- readLines(req_file)
+  
+  # Get installed packages
+  installed <- rownames(installed.packages())
+  
+  # Check and install missing ones
+  for (pkg in packages) {
+    if (!(pkg %in% installed)) {
+      message(paste("Installing", pkg, "..."))
+      install.packages(pkg)
+    } else {
+      message(paste(pkg, "is already installed."))
+    }
+  }
+  
+  message("✅ All required packages are installed and up to date.")
+}
